bdecd873f1121bfcf2dc3ce1d72c03d0a53cd54d
Axel Espinosa <axeldavid45@gmail.com>
2020-11-23 03:09:45 -0600

    fix: port fixed

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { join } = require('path')
 const app = express()
-const PORT = process.env.PORT | 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(join(__dirname, 'public')))
 
