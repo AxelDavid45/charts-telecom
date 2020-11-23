57dd26720559b054a8a5c0724622a72c128f1ea6
Axel Espinosa <axeldavid45@gmail.com>
2020-11-23 02:51:41 -0600

    feat: project structure changed to deploy

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { join } = require('path')
+const app = express()
+const PORT = process.env.PORT | 3000
+
+app.use(express.static(join(__dirname, 'public')))
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
